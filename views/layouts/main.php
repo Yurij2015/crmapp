d334919af684d5a81c6a075a0292ee4ea31c8b62
talgat065 <talgat06585@gmail.com>
2018-09-01 11:18:52 +0000

    change default layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,13 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+
+\yii\bootstrap\BootstrapAsset::register($this);
+\yii\web\YiiAsset::register($this);
+?>
+<?php $this->beginPage() ?>
 <!doctype html>
-<html lang="en">
+<html lang="<?=Yii::$app->language?>">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRM</title>
+    <meta charset="<?=Yii::$app->charset?>"/>
+    <title><?=Html::encode($this->title)?></title>
+    <?php $this->head() ?>
+    <?=Html::csrfMetaTags()?>
 </head>
 <body>
-    <?= $content;?>
+<?php $this->beginBody() ?>
+<div class="container">
+    <?=$content?>
+    <footer class="footer"><?=Yii::powered();?></footer>
+</div>
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
